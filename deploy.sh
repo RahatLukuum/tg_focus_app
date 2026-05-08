39e219ec70c43dafbf5278b056168e816e341696
den1shh <dengud79@gmail.com>
2026-05-08 04:56:11 +0300

    chore(deploy): switch VPS to 185.252.215.73

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 # === Конфигурация ===
 VPS_USER="root"
-VPS_HOST="94.131.109.193"
+VPS_HOST="185.252.215.73"
 VPS_PROJECT="/opt/tg_focus_app"
 VPS_WEBROOT="/var/www/tg-focus"
 BRANCH="denis-branch"
